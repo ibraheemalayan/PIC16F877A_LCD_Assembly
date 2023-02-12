c8eb8139a4b4a9092b94ae532abcef075565b8ed
Ibraheem Alyan <ibraheemalayan@gmail.com>
2023-02-12 17:23:58 +0200

    LCD Working

--- RealTime.X/main.asm
+++ RealTime.X/main.asm
@@ -5,41 +5,39 @@
 
 ; ----------- Data Area -----------
 
-RS	EQU 1		; Register select output bit
-E	EQU 2		; Enable display input
+LCD_RS_PIN	EQU 4		; Register select output bit
+LCD_E_PIN	EQU 5		; Enable display input
 
 ; 	Uses GPR 70 - 75 for LCD Data
 Timer1	EQU 0x70		; 1ms count register
 TimerX	EQU 0x71		; Xms count register
 Var	EQU 0x72		; Output variable
 Point	EQU 0x73		; Program table pointer
-Select	EQU 0x74		; Used to set or clear RS bit
-OutCod	EQU 0x75		; Temp store for output code
+TEMP	EQU 0x75		; Temp store for output code
 
 
 counter	EQU 0x20	; Counter register
 counterBlink	EQU 0x21; counter FOR Blinking register
 
-LCD_CURSOR	EQU 0x22; LCD_CURSOR register
-Location	EQU 0xC0; counter for LCD_CURSOR
+LCD_CSR	EQU 0x22; LCD_CSR register
+Location	EQU 0xC0; counter for LCD_CSR
 blinkDelay	EQU 0x50; Delay time for cursor blink :can be adjusted as needed
 
-CURSOR		EQU D'124'	; CURSOR: "  |  "
+CSR_LOC		EQU D'124'	; CSR_LOC: "  |  "
 WHITE		EQU D'32'	; WHITE space character
 
 ACHAR		EQU	D'65'; A
 ACHAR		EQU	D'65'; A
 currentCharReg	EQU	0x23;
-TEMP	EQU 0x24; variable for temporar data
 
 
 ; ---------------------------------
 ; ----------- Code Area -----------
 ; ---------------------------------
 
-ORG	0x0000 		; Start of program memory
-NOP			; For ICD mode
-GOTO start_exec
+    ORG	0x0000 		; Start of program memory
+    NOP			; For ICD mode
+    GOTO start_exec
 
 
 start_exec
@@ -62,7 +60,7 @@ start_exec
 	MOVWF	counter		; Store the value in the counter register
 
 	MOVLW	Location	; Load initial value of Location into W
-	MOVWF	LCD_CURSOR	; Store the value in the LCD_CURSOR register
+	MOVWF	LCD_CSR	; Store the value in the LCD_CSR register
 
 	MOVLW	ACHAR		; Load initial value of char 'A' To W
 	MOVWF	currentCharReg	; Store the value in the currentCharReg register
@@ -72,29 +70,35 @@ start_exec
 ; Initialise Timer0 for push button
 
 	BANKSEL OPTION_REG
-	
+
 	MOVLW	b'11011000'	; TMR0 initialisation code
 	MOVWF	OPTION_REG		; Int clock, no prescale	
 	
-	CALL LABEL_REACH ; announce reaching this line
+    ; this is causing everything to crash
+	; BANKSEL	INTCON		; select bank 0
+	; MOVLW	b'10100000'	; INTCON init. code
+	; MOVWF	INTCON		; Enable TMR0 interrupt
 
-	BANKSEL	INTCON		; Select bank 0
-	MOVLW	b'10100000'	; INTCON init. code
-	MOVWF	INTCON		; Enable TMR0 interrupt
+	
+
 
 
 ; Port & display setup
-	BANKSEL TRISD ; Select bank 1
+	BANKSEL TRISD ; select bank 1
 	CLRF TRISD ; Port D as output to LCD
 
-	BANKSEL PORTD ; Select bank 0
+	BANKSEL PORTD ; select bank 0
 	CLRF PORTD ; Clear display outputs
 
 
+	
+
 ;Port B defaults to inputs for the push button :using pin 1
 
-	CALL	initialize_display ; Initialise the display FOR LCD
+	CALL	SETUP_LCD ; Initialise the display FOR LCD
 	CALL	PUTEnterStringOnLCD
+	CALL LABEL_REACH ; announce reaching this line
+
 
 
 
@@ -158,76 +162,90 @@ loop_start	NOP
 		RETURN
 
 MOVERIGHTONLCD
-	BSF Select,RS ; Set to data mode
-	MOVF LCD_CURSOR, W ; Load the current position into the W register
+	BSF PORTD,LCD_RS_PIN ; Set to data mode
+	MOVF LCD_CSR, W ; Load the current position into the W register
 	ADDWF Point, F ; Calculate the address of the character to be shifted
 	MOVF Point, W ; Load the address of the character to be shifted into the W register
 	; CALL read ; Read the value at that address
-	MOVWF OutCod ; Store the value in OutCod
-	BCF Select,RS ; Set to command mode
+	MOVWF TEMP ; Store the value in TEMP
+	BCF PORTD,LCD_RS_PIN ; Set to command mode
 	MOVLW 0x14 ; Move cursor one position to the right
-	ADDWF LCD_CURSOR, F ; Update the cursor position
+	ADDWF LCD_CSR, F ; Update the cursor position
 	MOVLW 0x80 ; Move cursor to the second row, first column
-	ADDWF LCD_CURSOR, W ; Calculate the address of the second row, first column
+	ADDWF LCD_CSR, W ; Calculate the address of the second row, first column
 	MOVWF Point ; Store the address in Point
-	BSF Select,RS ; Set to data mode
-	MOVF OutCod, W ; Load the value stored in OutCod into the W register
-	CALL send ; Output the value to the display
-	BCF Select,RS ; Set to command mode
+	BSF PORTD,LCD_RS_PIN ; Set to data mode
+	MOVF TEMP, W ; Load the value stored in TEMP into the W register
+	CALL PulseWriteCharToLCD ; Output the value to the display
+	BCF PORTD,LCD_RS_PIN ; Set to command mode
 	MOVLW 0x10 ; Move cursor one position to the right
-	ADDWF LCD_CURSOR, F ; Update the cursor position
+	ADDWF LCD_CSR, F ; Update the cursor position
 	RETURN ; Return to the calling routine
 
 
 MOVELEFTONLCD
-	BSF Select,RS ; Set to data mode
-	MOVF LCD_CURSOR, W ; Load the current position into the W register
+	BSF PORTD,LCD_RS_PIN ; Set to data mode
+	MOVF LCD_CSR, W ; Load the current position into the W register
 	ADDWF Point, F ; Calculate the address of the character to be shifted
 	MOVF Point, W ; Load the address of the character to be shifted into the W register
 	; CALL read ; Read the value at that address
-	MOVWF OutCod ; Store the value in OutCod
-	BCF Select,RS ; Set to command mode
+	MOVWF TEMP ; Store the value in TEMP
+	BCF PORTD,LCD_RS_PIN ; Set to command mode
 	MOVLW 0x10 ; Move cursor one position to the left
-	ADDWF LCD_CURSOR, F ; Update the cursor position
+	ADDWF LCD_CSR, F ; Update the cursor position
 	MOVLW 0x80 ; Move cursor to the second row, first column
-	ADDWF LCD_CURSOR, W ; Calculate the address of the second row, first column
+	ADDWF LCD_CSR, W ; Calculate the address of the second row, first column
 	MOVWF Point ; Store the address in Point
-	BSF Select,RS ; Set to data mode
-	MOVF OutCod, W ; Load the value stored in OutCod into the W register
-	CALL send ; Output the value to the display
-	BCF Select,RS ; Set to command mode
+	BSF PORTD,LCD_RS_PIN ; Set to data mode
+	MOVF TEMP, W ; Load the value stored in TEMP into the W register
+	CALL PulseWriteCharToLCD ; Output the value to the display
+	BCF PORTD,LCD_RS_PIN ; Set to command mode
 	MOVLW 0x14 ; Move cursor one position to the left
-	ADDWF LCD_CURSOR, F ; Update the cursor position
+	ADDWF LCD_CSR, F ; Update the cursor position
 	RETURN ; Return to the calling routine
 
+
 PUTEnterStringOnLCD 
-	BCF Select,RS ; command mode
-	MOVLW 0x80    ; move cursor to first line, first column
-	CALL send     ; output it to display
-	BSF Select,RS ; data mode
+
+	MOVLW	0x80		; move cursor to first line, first column
+	CALL	PulseWriteCmdToLCD ; send command
+
 	MOVLW 'E';
-	CALL send;
+	CALL PulseWriteCharToLCD;
+
 	MOVLW 'n';
-	CALL send; 
+	CALL PulseWriteCharToLCD;
+
 	MOVLW 't';
-	CALL send;
+	CALL PulseWriteCharToLCD;
+
 	MOVLW 'e'; 
-	CALL send; 
+	CALL PulseWriteCharToLCD;
+
 	MOVLW 'r';
-	CALL send;
+	CALL PulseWriteCharToLCD;
+
 	MOVLW ' ';
-	CALL send;
+	CALL PulseWriteCharToLCD;
+
 	MOVLW 'S';
-	CALL send;
+	CALL PulseWriteCharToLCD;
+
 	MOVLW 't';
-	CALL send;
+	CALL PulseWriteCharToLCD;
+
 	MOVLW 'r';
-	CALL send; 
+	CALL PulseWriteCharToLCD;
+
 	MOVLW 'i';
-	CALL send;
+	CALL PulseWriteCharToLCD;
+
  	MOVLW 'n'; 
-	CALL send; 
+	CALL PulseWriteCharToLCD;
+
 	MOVLW 'g';
+	CALL PulseWriteCharToLCD;
+
 	RETURN
 	
 	
@@ -237,19 +255,19 @@ PUTEnterStringOnLCD
 LOOPTOLOCATION
 	CALL	BLINK		;  blinking the cursor at the Location to Write before pushing the button 
 	CALL	LOOPtoIndex	; changing the char
-	INCF	LCD_CURSOR, F
+	INCF	LCD_CSR, F
 	RETURN
 
 LOOPtoIndex
 	CALL	PINSTABLE
 	CALL	waitINT
 	CALL	IsReleased
-	BCF	Select,RS	; command mode
-	MOVWF	LCD_CURSOR	; Selection Location
-	CALL	send		; send command 
-	BSF	Select,RS	; data mode
+	BCF	PORTD,LCD_RS_PIN	; command mode
+	MOVWF	LCD_CSR	; selection Location
+	CALL	PulseWriteCmdToLCD ; send command 
+	BSF	PORTD,LCD_RS_PIN	; data mode
 	MOVF currentCharReg, W	;Write char A on W
-	CALL	send		; send Data
+	CALL	PulseWriteCharToLCD		; send Data
 	CALL	TIMER2S
 	CALL	WaitButton	;Increment when button pressed
 	RETURN
@@ -296,10 +314,10 @@ PRESED
 	CALL	IsReleased
 
 INCREMENTCHAR
-	BCF	Select,RS	; command mode
-	MOVWF	LCD_CURSOR	; Selection Location
-	CALL	send		; send command 
-	BSF	Select,RS	; data mode
+	BCF	PORTD,LCD_RS_PIN	; command mode
+	MOVWF	LCD_CSR	; selection Location
+	CALL	PulseWriteCmdToLCD ; send command 
+	BSF	PORTD,LCD_RS_PIN	; data mode
 
 CheakForZ
 	MOVF	currentCharReg, W; Move the contents of currentCharReg to W
@@ -324,7 +342,7 @@ SKIP
 
 MOVETOLCD
 	MOVF currentCharReg, W	; Write char on W
-	CALL	send		; send Data
+	CALL	PulseWriteCharToLCD		; send Data
 	CALL	TIMER2S		; Old value for timer 2 to mkae 2s
 	GOTO	WaitButton	; To Update the same location
 		
@@ -339,7 +357,44 @@ IsReleased
 	BTFSS	PORTB,1		; Check step button
 	GOTO	IsReleased	; and wait until released
 	RETURN
-		
+
+PulseWriteCmdToLCD
+	MOVWF TEMP
+	SWAPF TEMP
+	MOVF TEMP, W
+	ANDLW 0x0f
+	MOVWF PORTD
+	BCF PORTD, LCD_RS_PIN ;Clearing the LCD_RS_PIN Register for command mode
+	call PULSE_LCD_E_PIN
+	;call onems
+	SWAPF TEMP
+	movf TEMP, W
+	ANDLW	0x0f
+	MOVWF PORTD
+	BCF PORTD, LCD_RS_PIN ;Clearing the LCD_RS_PIN Register for command mode
+	call PULSE_LCD_E_PIN
+	RETURN
+
+;--------------------------------------------------------------
+;	Send a command byte in two nibbles from RB4 - RB7
+;	Receives command in W, uses PulseE and Onems
+;--------------------------------------------------------------
+PulseWriteCharToLCD
+    MOVWF TEMP
+    SWAPF TEMP
+    MOVF TEMP, W
+    ANDLW 0x0F ; Clear low nybble
+    MOVWF PORTD
+    BSF PORTD, LCD_RS_PIN ; set character mode on RS
+    call PULSE_LCD_E_PIN ; clock LCD
+    call onems
+    SWAPF TEMP
+    MOVF TEMP, W
+    ANDLW	0x0F ; Clear high nybble
+    MOVWF PORTD
+    BSF PORTD, LCD_RS_PIN 
+    call PULSE_LCD_E_PIN 
+    RETURN
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;End Loop senario;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -352,12 +407,12 @@ BLINK
 	BTFSS	PORTB,1		; Double cheak  button
 	RETURN			;
 
-	BCF	Select,RS	; command mode
-	MOVWF	LCD_CURSOR	; Selection Location
-	CALL	send		; send command 
-	BSF	Select,RS	; data mode
-	MOVLW	CURSOR		; |
-	CALL	send		; send CURSOR
+	BCF	PORTD,LCD_RS_PIN	; command mode
+	MOVWF	LCD_CSR	; selection Location
+	CALL	PulseWriteCmdToLCD ; send command 
+	BSF	PORTD,LCD_RS_PIN	; data mode
+	MOVLW	CSR_LOC		; |
+	CALL	PulseWriteCmdToLCD		; send CSR_LOC
 	
 
 	MOVLW	blinkDelay	; Load the delay value into W
@@ -365,12 +420,12 @@ BLINK
 	BTFSS	PORTB,1		; Double cheak  button
 	RETURN			;
 
-	BCF	Select,RS	; command mode
-	MOVWF	LCD_CURSOR	; Selection Location
-	CALL	send		; send command 
-	BSF	Select,RS	; data mode
+	BCF	PORTD,LCD_RS_PIN	; command mode
+	MOVWF	LCD_CSR	; selection Location
+	CALL	PulseWriteCmdToLCD ; send command 
+	BSF	PORTD,LCD_RS_PIN	; data mode
 	MOVLW	WHITE		; 
-	CALL	send		; WHITE
+	CALL	PulseWriteCmdToLCD		; WHITE
 	BTFSC	PORTB,1		; Test button
 	GOTO	BLINK		; Loop back
 	RETURN			; counterBlinking to Zero when clicking the button
@@ -391,84 +446,58 @@ DELAY_LOOP
 ;--------------------------------------------------------------
 onems	MOVLW	D'249'		; Count for 1ms delay 
 	MOVWF	Timer1		; Load count
-loop1	NOP			; Pad for 4 cycle loop
+LP	NOP			; Pad for 4 cycle loop
 	DECFSZ	Timer1		; Count
-	GOTO	loop1		; until Z
-	RETURN			; and finish
-
-;--------------------------------------------------------------
-;	Delay Xms
-;	Receives count in W, uses Onems
-;--------------------------------------------------------------
-xms	MOVWF	TimerX		; Count for X ms
-loopX	CALL	onems		; Delay 1ms
-	DECFSZ	TimerX		; Repeat X times 
-	GOTO	loopX		; until Z
+	GOTO	LP		; until Z
 	RETURN			; and finish
 
 ;--------------------------------------------------------------
 ;	Generate data/command clock siganl E
 ;--------------------------------------------------------------
-pulseE	BSF	PORTD,E		; Set E high
+PULSE_LCD_E_PIN	BSF	PORTD, LCD_E_PIN		; Set E high
 	CALL	onems		; Delay 1ms
-	BCF	PORTD,E		; Reset E low
+	BCF	PORTD, LCD_E_PIN		; Reset E low
 	CALL	onems		; Delay 1ms
 	RETURN			; done
 
-;--------------------------------------------------------------
-;	Send a command byte in two nibbles from RB4 - RB7
-;	Receives command in W, uses PulseE and Onems
-;--------------------------------------------------------------
-send	MOVWF	OutCod		; Store output code
-	ANDLW	0F0		; Clear low nybble
-	MOVWF	PORTD		; Output high nybble
-	BTFSC	Select,RS	; Test RS bit
-	BSF	PORTD,RS	; and set for data
-	CALL	pulseE		; and clock display register
-	CALL	onems		; wait 1ms for display
-
-	SWAPF	OutCod		; Swap low and high nybbles 
-	MOVF	OutCod,W	; Retrieve output code
-	ANDLW	0F0		; Clear low nybble
-	MOVWF	PORTD		; Output low nybble
-	BTFSC	Select,RS	; Test RS bit
-	BSF	PORTD,RS	; and set for data
-	CALL	pulseE		; and clock display register
-	CALL	onems		; wait 1ms for display
-	RETURN			; done
 
 ;--------------------------------------------------------------
 ;	Initialise the display
-;	Uses Send
 ;--------------------------------------------------------------
-initialize_display	MOVLW	D'100'		; Load count for 100ms delay
-	CALL	xms		; and wait for display start
-	MOVLW	0F0		; Mask for select code
-	MOVWF	Select		; High nybble not masked
+SETUP_LCD	
 
-	MOVLW	0x30		; Load initial nibble
-	MOVWF	PORTD		; and output it to display
-	CALL	pulseE		; Latch initial code
-	MOVLW	D'5'		; Set delay 5ms
-	CALL	xms		; and wait
-	CALL	pulseE		; Latch initial code again
-	CALL	onems		; Wait 1ms
-	CALL	pulseE		; Latch initial code again
-	BCF	PORTD,4		; Set 4-bit mode
-	CALL	pulseE		; Latch it
 	
-	MOVLW	0x28		; Set 4-bit mode, 2 lines
-	CALL	send		; and send code
-	MOVLW	0x08		; Switch off display
-	CALL	send		; and send code
+
+	CLRF	PORTD		; and output it to display
+
+	MOVLW	0x40		; wait for display start
+	CALL	DELAY_W
+	
+	
+	MOVLW	0x02	; Set 4-bit mode
+	CALL	PulseWriteCmdToLCD ; send command
+	
+	MOVLW	0x28		; Set 5 by 7 mode
+	CALL	PulseWriteCmdToLCD ; send command
+
+	; TODO remove
+	; MOVLW	0x08		; Switch off display
+	; CALL	PulseWriteCmdToLCD ; send command
+
 	MOVLW	0x01		; Code to clear display
-	CALL	send		; and send code
-	MOVLW	0x06		; Enable cursor auto inc  
-	CALL	send		; and send code
-	MOVLW	0x80		; Zero display address
-	CALL	send		; and send code
+	CALL	PulseWriteCmdToLCD ; send command
+
+	MOVLW	0x0E		; Display ON and cursor blinking
+    CALL	PulseWriteCmdToLCD	; send command
+
+	; MOVLW	0x06		; Enable cursor auto inc  
+	; CALL	PulseWriteCmdToLCD ; send command
+
+	MOVLW	0x80		; move cursor to first line, first column
+	CALL	PulseWriteCmdToLCD ; send command
+
 	MOVLW	0x0C		; Turn on display  
-	CALL	send		; and send code
+	CALL	PulseWriteCmdToLCD ; send command
 
 	RETURN			; Done
 
